ef11e52cf00c1c4e11092a8d1117ef7b17a1ece4
Wiktor <wktr.chojnacki@gmail.com>
2023-04-14 11:24:07 +0200

    Input cancel button

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -23,15 +23,45 @@ export const SearchBox = ({}: SearchBoxProps) => {
 
   return (
     <div className={styles.searchBox}>
-      <input
-        type="text"
-        placeholder="Search for a user"
-        ref={inputRef}
-        value={phrase}
-        onChange={(e) => {
-          setPhrase(e.target.value);
-        }}
-      />
+      <div className={styles.inputContainer}>
+        <input
+          type="text"
+          placeholder="Search for a user"
+          ref={inputRef}
+          value={phrase}
+          onChange={(e) => {
+            setPhrase(e.target.value);
+          }}
+        />
+        {phrase.length > 0 && (
+          <div className={styles.cancelButton}>
+            <svg
+              onClick={() => setPhrase("")}
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18 6L6 18"
+                stroke="#BDBDBD"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M6 6L18 18"
+                stroke="#BDBDBD"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        )}
+      </div>
+
       <Button
         caption="Search"
         clickHandler={() => {
